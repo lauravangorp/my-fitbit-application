2b22f9c33afe04c7d7d02fe97af06de0d08a39b4
lauravangorp <lauravangorp@users.noreply.github.com>
2020-10-26 09:13:21 +0100

    voorbeeld

--- app/index.js
+++ app/index.js
@@ -1,21 +1,9 @@
-import * as messaging from "messaging";
-import document from 'document';
+import { init as initState } from './state';
+import { init as initCommunication } from './communication';
+import { init as initNavigation, switchPage } from './navigation';
+import router from './router';
 
-messaging.peerSocket.addEventListener("open", (evt) => {
-  sendMessage();
-});
-
-function sendMessage() {
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send({name: 'mynumber', value: 100});
-  }
-}
-
-messaging.peerSocket.addEventListener("message", (evt) => {
-  console.log(evt.data);
-  console.log(JSON.stringify(evt.data));
-
-  if(evt.data.name === "mynumber" && evt.data.value > 20){
-    document.getElementById('background').style.fill = 'blue';
-  }
-});
+initState();
+initCommunication();
+initNavigation(router);
+switchPage('index');
---
+++ app/pages/detail.js
@@ -0,0 +1,35 @@
+import document from 'document';
+import { getWeather } from '../communication';
+import { getStateItem, setStateCallback, removeStateCallback } from '../state';
+
+let $button = null;
+let $temperature = null;
+
+function doSomething() {
+  console.log('hallo detail');
+}
+
+function draw() {
+  $temperature.text = getStateItem('temperature');
+}
+
+export function destroy() {
+  console.log('destroy detail page');
+  $temperature = null;
+  $button = null;
+  removeStateCallback();
+}
+
+export function init() {
+  console.log('init detail page');
+  $temperature = document.getElementById('temperature');
+  $button = document.getElementById('back-button');
+  $button.onclick = () => {
+    destroy();
+    document.history.back();
+  };
+
+  doSomething();
+  getWeather();
+  setStateCallback(draw);
+}
